e4fe22a2ebb0782cbe879535594fc8478bfa3a61
simplyadrian <simplyadrian@gmail.com>
2015-04-24 16:06:42 -0500

    ISE-366 added helper and idempotence check to windows recipe.

---
+++ libraries/timezone_helper.rb
@@ -0,0 +1,11 @@
+module TimezoneNativex
+  module Helper
+
+  include Chef::Mixin::ShellOut
+  
+    def timezone_set?
+      check = shell_out.new("powershell.exe -command [system.timezoneinfo]::local").run_command
+      check.stdout.include? ("#{node['timezone-nativex']['windows_tz_name']}")
+    end
+  end
+end
--- recipes/windows.rb
+++ recipes/windows.rb
@@ -7,8 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
+Chef::Provider::PowershellScript.send(:include, TimezoneNativex::Helper)
+
 powershell_script "set windows timezone" do
   code <<-EOH
   tzutil.exe /s "#{node['timezone-nativex']['windows_tz_name']}"
   EOH
+  not_if {timezone_set?}
 end
