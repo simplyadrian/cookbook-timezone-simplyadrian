aa4e5b8268ede8b462661983c7242d15fbba9c34
simplyadrian <simplyadrian@gmail.com>
2015-04-30 11:46:49 -0500

    ISE-366 update

--- recipes/windows.rb
+++ recipes/windows.rb
@@ -13,5 +13,5 @@ powershell_script "set windows timezone" do
   code <<-EOH
   tzutil.exe /s "#{node['timezone-nativex']['windows_tz']}"
   EOH
-  not_if { timezone_set? }
+  #not_if { timezone_set? } #each_char method on String is not available by default in Ruby 1.8.6 and you will be presented with a NoMethodError.
 end
