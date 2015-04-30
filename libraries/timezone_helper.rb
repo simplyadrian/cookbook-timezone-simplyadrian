2df26c622ae8af7189bde406f918ad9d596f89a5
simplyadrian <simplyadrian@gmail.com>
2015-04-30 11:32:01 -0500

    ISE-366 update

--- libraries/timezone_helper.rb
+++ libraries/timezone_helper.rb
@@ -2,11 +2,11 @@ module TimezoneNativex
   module Helper
 
   include Chef::Mixin::ShellOut
-  require 'jcode' #each_char method on String is not available by default in Ruby 1.8.6 and you will be presented with a NoMethodError.
+  #require 'jcode' if RUBY_VERSION < '1.9' #each_char method on String is not available by default in Ruby 1.8.6 and you will be presented with a NoMethodError.
   
     def timezone_set?
       check = shell_out.new("powershell.exe -command [system.timezoneinfo]::local").run_command
-      check.stdout.include? ("#{node['timezone-nativex']['windows_tz']}")
+      check.stdout.grep("#{node['timezone-nativex']['windows_tz']}")
     end
   end
 end
